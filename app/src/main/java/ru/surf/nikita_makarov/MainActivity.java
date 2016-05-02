8af19d369013a463cddf27580c32a6bdda0accb5
Nikita <nikitj@rambler.ru>
2016-05-02 10:33:40 +0400

    Added scrolling of the text. No paddings in RelativeLayout.

--- app/src/main/java/ru/surf/nikita_makarov/MainActivity.java
+++ app/src/main/java/ru/surf/nikita_makarov/MainActivity.java
@@ -2,6 +2,7 @@ package ru.surf.nikita_makarov;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = (TextView) findViewById(R.id.text);
+        text.setText("9:39:51 Gradle sync started\n" +
+                "9:40:55 Gradle sync completed\n" +
+                "9:40:57 Executing tasks: [:app:generateDebugSources, :app:generateDebugAndroidTestSources, :app:mockableAndroidJar, :app:prepareDebugUnitTestDependencies]\n" +
+                "9:41:47 Gradle build finished in 51s 457ms\n" +
+                "9:52:29 Unregistered VCS root detected\n" +
+                "        The directory C:\\Users\\user\\Desktop\\Surf\\Colorlist is under Git, but is not registered in the Settings.\n" +
+                "        Add root  Configure  Ignore\n" +
+                "9:58:22 34 files committed: Project files are copied into reprository, excluding .gitignore list.\n" +
+                "9:58:41 Push successful: Pushed 1 commit to origin/master" + "9:39:51 Gradle sync started\n" +
+                "9:40:55 Gradle sync completed\n" +
+                "9:40:57 Executing tasks: [:app:generateDebugSources, :app:generateDebugAndroidTestSources, :app:mockableAndroidJar, :app:prepareDebugUnitTestDependencies]\n" +
+                "9:41:47 Gradle build finished in 51s 457ms\n" +
+                "9:52:29 Unregistered VCS root detected\n" +
+                "        The directory C:\\Users\\user\\Desktop\\Surf\\Colorlist is under Git, but is not registered in the Settings.\n" +
+                "        Add root  Configure  Ignore\n" +
+                "9:58:22 34 files committed: Project files are copied into reprository, excluding .gitignore list.\n" +
+                "9:58:41 Push successful: Pushed 1 commit to origin/master" + "9:39:51 Gradle sync started\n" +
+                "9:40:55 Gradle sync completed\n" +
+                "9:40:57 Executing tasks: [:app:generateDebugSources, :app:generateDebugAndroidTestSources, :app:mockableAndroidJar, :app:prepareDebugUnitTestDependencies]\n" +
+                "9:41:47 Gradle build finished in 51s 457ms\n" +
+                "9:52:29 Unregistered VCS root detected\n" +
+                "        The directory C:\\Users\\user\\Desktop\\Surf\\Colorlist is under Git, but is not registered in the Settings.\n" +
+                "        Add root  Configure  Ignore\n" +
+                "9:58:22 34 files committed: Project files are copied into reprository, excluding .gitignore list.\n" +
+                "9:58:41 Push successful: Pushed 1 commit to origin/master" + "9:39:51 Gradle sync started\n" +
+                "9:40:55 Gradle sync completed\n" +
+                "9:40:57 Executing tasks: [:app:generateDebugSources, :app:generateDebugAndroidTestSources, :app:mockableAndroidJar, :app:prepareDebugUnitTestDependencies]\n" +
+                "9:41:47 Gradle build finished in 51s 457ms\n" +
+                "9:52:29 Unregistered VCS root detected\n" +
+                "        The directory C:\\Users\\user\\Desktop\\Surf\\Colorlist is under Git, but is not registered in the Settings.\n" +
+                "        Add root  Configure  Ignore\n" +
+                "9:58:22 34 files committed: Project files are copied into reprository, excluding .gitignore list.\n" +
+                "9:58:41 Push successful: Pushed 1 commit to origin/master" + "9:39:51 Gradle sync started\n" +
+                "9:40:55 Gradle sync completed\n" +
+                "9:40:57 Executing tasks: [:app:generateDebugSources, :app:generateDebugAndroidTestSources, :app:mockableAndroidJar, :app:prepareDebugUnitTestDependencies]\n" +
+                "9:41:47 Gradle build finished in 51s 457ms\n" +
+                "9:52:29 Unregistered VCS root detected\n" +
+                "        The directory C:\\Users\\user\\Desktop\\Surf\\Colorlist is under Git, but is not registered in the Settings.\n" +
+                "        Add root  Configure  Ignore\n" +
+                "9:58:22 34 files committed: Project files are copied into reprository, excluding .gitignore list.\n" +
+                "9:58:41 Push successful: Pushed 1 commit to origin/master" + "9:39:51 Gradle sync started\n" +
+                "9:40:55 Gradle sync completed\n" +
+                "9:40:57 Executing tasks: [:app:generateDebugSources, :app:generateDebugAndroidTestSources, :app:mockableAndroidJar, :app:prepareDebugUnitTestDependencies]\n" +
+                "9:41:47 Gradle build finished in 51s 457ms\n" +
+                "9:52:29 Unregistered VCS root detected\n" +
+                "        The directory C:\\Users\\user\\Desktop\\Surf\\Colorlist is under Git, but is not registered in the Settings.\n" +
+                "        Add root  Configure  Ignore\n" +
+                "9:58:22 34 files committed: Project files are copied into reprository, excluding .gitignore list.\n" +
+                "9:58:41 Push successful: Pushed 1 commit to origin/master");
     }
 }
